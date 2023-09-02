cceba82a5df8bceff02ca11b4449efee3b9148f7
= <=>
2023-09-02 08:15:15 +0700

    first commit

---
+++ zahra.js
@@ -0,0 +1,15 @@
+const inputPassword = document.getElementById("input-password");
+const errorMessage = document.getElementById("error-message");
+const submitButton = document.getElementById("submit-button");
+
+ submitButton.addEventListener("click", () => {
+const password = inputPassword.value;
+
+if (password.length > 7) {
+errorMessage.textContent = "Password Valid";
+errorMessage.style.color = "green";
+} else {
+    errorMessage.textContent = "password terlalu pendek";
+    errorMessage.style.color = "red";
+}
+});
